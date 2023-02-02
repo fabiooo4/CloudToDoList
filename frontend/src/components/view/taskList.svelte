99632563ecbab82b9f9bf1bce413b6c960788f1e
fabiooo4 <fabioirimie@gmail.com>
2023-02-02 19:34:47 +0100

    fixed taskList

--- frontend/src/components/view/taskList.svelte
+++ frontend/src/components/view/taskList.svelte
@@ -5,8 +5,6 @@
 
   import { toDoList } from '../controller/store.js';
   import { userId } from '../controller/store.js';
-
-
 </script>
 
 <h1 class="text-8xl text-center font-extrabold m-4">ToDo List</h1>
@@ -17,13 +15,9 @@
 </div>
 
 <div class='flex flex-row flex-wrap justify-center items-center w-screen pb-10'>
-  {#if $toDoList.length > 0}
-    {#each $toDoList as task}
-      {#if task.userId == $userId}
-        <Task {task}/>
-      {/if}
-    {/each}
-  {:else}
-    <h2 class='text-2xl text-center font-bold m-4'>No tasks found...</h2>
-  {/if}
+  {#each $toDoList as task}
+    {#if task.userId == $userId}
+      <Task {task}/>
+    {/if}
+  {/each}
 </div>
